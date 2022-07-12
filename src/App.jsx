45ae2f5626330b2a0ac22b4cd8c83a0d85a03944
Marina Font <marinafoper.1998@gmail.com>
2022-07-12 19:00:26 +0200

    Funcionamiento base

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,72 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import './App.css'
 import { useSyncExternalStore } from "react";
- 
- 
+import { BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import UserData from './User';
+
+
 export default function App() {
- 
-  const [data, setData] = useState([]);
+
+  const readPartners = () => {
+    const partners = localStorage.getItem("partners");
+    return partners !== null ? JSON.parse(partners) : [];
+  }
+
+  const [data, setData] = useState(readPartners);
 
   const newUser = async () => {
+    //petición a la api + guardar datos en un objeto user
     const res = await fetch('https://randomuser.me/api/'); //nos devuelve un json con los datos de la API
-    const user = await res.json();
-    console.log(user);
-
-    // var newArray = user.results.map(function(item){
-    //   var newParner = {
-    //     id: item.id.name,
-    //     name: item.name.first,
-    //     gender: item.gender,
-    //   }
-    //   return newParner;
-    // })
-    // console.log(newArray)
+    const user = await res.json()
 
     setData(prevState =>([...prevState,user.results[0]]));
-    console.log
+    console.log(data);
   };
 
+  const cleanUser = () => {
+    setData(data => data=[]);
+    localStorage.clear()
+    localStorage.removeItem("partners");
+  }
+
   useEffect( ()=> {
-    console.log(data)
-  },[newUser]);
- 
+    localStorage.setItem("partners",JSON.stringify(data));
+  },[newUser,]);
+
+  // const prova = 'hola';
+
   return (
     <div>
-      <h1>Find your partner: </h1>
-      <button onClick={newUser}>Match</button>
-      {data.map(item => (
-        <p key={item.id.name}>{item.name.first}</p>
-      ))}
+      <h1>Find your random partner:</h1>
+      <div id="buttons">
+        <button id="match" onClick={newUser}>Match</button>
+        <button id="delete" onClick={cleanUser}>Clean</button>
+      </div>          
+        <Router>
+        <ul>
+          {data.map(item => (
+            <div key={item.id.value} className="partners">
+              <Link to={`/user/${item.name.first} `}>
+                <p>{item.name.first}</p>
+                <p>{item.email} </p>
+              </Link>
+            </div>
+          ))}
+          </ul>
+            <Routes>
+              <Route path="/user/:name" element={<UserData/>} />
+              <Route path="/user" element={<UserData/>} />
+            </Routes>
+        </Router>
     </div>
   );
 }
+
+
+// function Users() {
+//   return (
+//     <div>
+//       <h1>Hola</h1>
+//     </div>
+//   )
+// }
